c3866d36b12b9ec0e7ef65b6e738905ea115e46e
Cryptec <neupi92@web.de>
2021-09-15 11:53:18 +0200

    fix map

--- src/components/UI/Footer/Footer.js
+++ src/components/UI/Footer/Footer.js
@@ -5,8 +5,6 @@ import Section from '../../../HOC/Section';
 import Link from '../Link/Link';
 //import Datenschutz from "../../sections/info/datenschutz";
 
-import {showMap, hideMap} from '../../handler'
-
 
 class Footer extends Component {
   constructor() {
@@ -14,15 +12,13 @@ class Footer extends Component {
     this.state = {
       email: "",
       map: false
-    };   
+    }
   } 
 
 toggleMapClose = () => {
-    hideMap()
     this.setState({ map: false })
 }
 toggleMapOpen = () => {
-  showMap()
   this.setState({map: true})
 }
 
@@ -34,19 +30,20 @@ toggleMap = () => {
 
     return (   
       <footer className=''>
-      <Section id='map'>
+      {this.state.map ? <Section id='map'>
       <div className="MapMaker">
         <iframe
           title='mapMarker'
           src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5259.287594939124!2d13.50777364783047!3d48.76959825723895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4774ee54f83e1787%3A0x9156c98686567afe!2sGewerbegebiet%20Kumreut-S%C3%BCd%2C%20%C3%96dholz%201%2C%2094133%20R%C3%B6hrnbach!5e0!3m2!1sen!2sde!4v1613393526760!5m2!1sen!2sde'
           width='100%'
           height='450'
+          display='none'
           frameBorder='0'
           style={{ border: 0, marginBottom: '-7px'}}
           allowFullScreen
         />
       </div>
-    </Section>
+    </Section> : null }
       <div className='container text-light pt-5'>
         <div className='row'>
           
@@ -72,7 +69,7 @@ toggleMap = () => {
                 <a href='https://www.ebay.de/str/teilezirkus' className='text-light'>
                   <i className='fab fa-ebay mr-4' />
                 </a>
-                <i className='fas fa-map-marked-alt' style={{cursor: "pointer"}} onClick={this.toggleMap} />
+                <div className='fas fa-map-marked-alt' style={{cursor: "pointer"}} onClick={this.toggleMap} />
               </div>
             </div>
           </div>
--- src/components/sections/Sections.js
+++ src/components/sections/Sections.js
@@ -4,7 +4,6 @@ import Home from './Home/Home';
 import About from './About/About';
 import Service from './Service/Service';
 import Facts from './Facts/Facts';
-//import Blog from './Blog/Blog';
 import Contact from './Contact/Contact';
 
 const sections = () => {
